a23ab021e9ef361e6fccd2673d7f922cb21fa5ab
AMA2581 <amir.abravesh.ama@gmail.com>
2024-02-04 02:17:50 +0330

    Initial Commit

---
+++ CamAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CamAI
+//
+//  Created by Amir Mahdi Abravesh on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
