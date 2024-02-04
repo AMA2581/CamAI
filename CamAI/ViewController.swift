75b6c9d3092fc8cadd04a250332a517515ca146d
AMA2581 <amir.abravesh.ama@gmail.com>
2024-02-04 03:37:22 +0330

    first commit

--- CamAI/ViewController.swift
+++ CamAI/ViewController.swift
@@ -5,15 +5,66 @@
 //  Created by Amir Mahdi Abravesh on 2/4/24.
 //
 
+import CoreML
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView! // add weak after formating
+
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera // lets user to use a picture from camera
+        imagePicker.allowsEditing = false // no editing alowed
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model Failed to Process Image")
+            }
+            
+            if let firstResult = results.first {
+                var showStr = firstResult.description
+                showStr.removeSubrange(showStr.startIndex..<showStr.index(showStr.startIndex, offsetBy: 126))
+                showStr = showStr.replacingOccurrences(of: "\"", with: "")
+                self.navigationItem.title = showStr
+//                print(showStr)
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
-
